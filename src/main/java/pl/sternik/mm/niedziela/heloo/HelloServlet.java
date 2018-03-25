9d0c8b727db3408ed4a118848c831a9d7551a459
Marta Morawiec <marta.morawiec@edu.uekat.pl>
2018-03-25 13:05:23 +0200

    sternik-niedziela

---
+++ src/main/java/pl/sternik/mm/niedziela/heloo/HelloServlet.java
@@ -0,0 +1,19 @@
+package pl.sternik.mm.niedziela.heloo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet{
+    void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getOutputStream().println("Mój pierwszy servlet");
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getOutputStream().println("Mój pierwszy servlet z obsluga POST ->"
+                + request.getParameter("param1"));
+    }
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="/hello">Mów do mnie!</a>
+</body>
+</html>
